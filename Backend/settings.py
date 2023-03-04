42e6a38c4b68abb254c2cf290f638f5e88752c79
Logan Wagner <loganawagner@gmail.com>
2023-03-04 08:28:12 -0600

    linking template dir

--- Backend/settings.py
+++ Backend/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'FrontEnd'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
