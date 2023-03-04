c9a3a42642dc6214ed3c15cbcba595193ddbb0df
Jack Vest <jackc.vest@gmail.com>
2023-03-04 13:15:30 -0600

    haha silly changes

--- Backend/game.py
+++ Backend/game.py
@@ -1,11 +1,14 @@
 import state
+import pac
 
 class Game:
     def __init__(self, player1, player2, t):
-        self.p1 = player1   #id for player 1
-        self.p2 = player2   #id for player 2
-        self.state = state.State()    #object of state class 
+        self.p1 = pac.Player(1)   #id for player 1
+        self.p2 = pac.Player(2)   #id for player 2
+        self.p1.set_color(0)
+        self.p2.set_color(1)
 
+        self.state = state.State()    #object of state class 
     
 print("hello how does this work")
 
