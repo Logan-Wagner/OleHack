f7bd897db8cfe63c3248d3d9bc6329af07e27632
Logan Wagner <loganawagner@gmail.com>
2023-03-04 15:33:47 -0600

    new server-client communication

--- ourproject/ourapp/views.py
+++ ourproject/ourapp/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
+from . import piece
 
 def index(request):
     return render(request, 'index.html')
+
+def newGame(request):
+    pieces = []
+    for i in range(0,8):
+        pieces.append(piece.Piece(i, 7, 0))
+        pieces.append(piece.Piece(i, 0, 1))
+    context = {
+        'pieces': pieces,
+    }
+    return render(request, 'index.html', context)
