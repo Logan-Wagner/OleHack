125af9a01c180c5e563dd8db6d36e138f7ef81d5
Brianna Gorny <briannagorny@Briannas-MacBook-Air.local>
2023-03-04 11:16:14 -0600

    little change

--- Backend/game.py
+++ Backend/game.py
@@ -4,8 +4,7 @@ class Game:
     def __init__(self, player1, player2, t):
         self.p1 = player1   #id for player 1
         self.p2 = player2   #id for player 2
-        self.turn = t       #bool, 0 for player 1 and 1 for player 2
-        state.self.state    #object of state class 
+        self.state = state.State()    #object of state class 
 
     
 print("hello how does this work")
