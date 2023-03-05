2c786005cd946ef185538151d488efff054d6765
Logan Wagner <loganawagner@gmail.com>
2023-03-05 02:02:02 -0600

    many updates

--- ourproject/ourapp/game.py
+++ ourproject/ourapp/game.py
@@ -3,11 +3,14 @@ from . import pac
 
 class Game:
     def __init__(self, player1, player2, deck):
-        self.p1 = pac.Player(1)   #id for player 1
-        self.p2 = pac.Player(2)   #id for player 2
+        self.p1 = player1
+        self.p2 = player2
         self.p1.set_color(0)
         self.p2.set_color(1)
-        self.state = state.State(deck)    #object of state class 
-    
-print("hello how does this work")
+        self.state = state.State(deck)    #object of state class
 
+    def get_player(self, number):
+        if number == 1:
+            return self.p1
+        else:
+            return self.p2
--- ourproject/ourapp/models.py
+++ ourproject/ourapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    Player1 = models.IntegerField()
+    Player2 = models.IntegerField()
+    P1Hand = models.TextField()
+    P2Hand = models.TextField()
+    WhitePieces = models.TextField()
+    BlackPieces = models.TextField()
+    Move = models.IntegerField()
+    CurrentWinCondition = models.IntegerField()
+    Deck = models.TextField()
